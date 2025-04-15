7dd5409cc65ac558eed146f421bb3dd4f240cbac
viktor <sterilework@gmail.com>
2025-04-15 16:01:25 +0300

    test: fix expected result

--- src/pw_05/test_01.sh
+++ src/pw_05/test_01.sh
@@ -20,7 +20,7 @@ $DIR_PATH/task_01 1.23 2.33 5 5.78
 printFormat "- Expected: 3.8621" $GRAY
 printFormat "- Execution 3: ./task_01 1.23 2.33 5.78 9" $GRAY
 $DIR_PATH/task_01 1.23 2.33 5.78 9
-printFormat "- Expected: 4.86082" $GRAY
+printFormat "- Expected: 3.86082" $GRAY
 printFormat -n "Case 03:" $BLUE $BOLD $UNDERLINE
 printFormat " 1 > 2 > 3 > 4 > 6 > 10 > 12" $BLUE
 printFormat "- Execution 5: ./task_01 -1.23 2.33 5.78 9" $GRAY
